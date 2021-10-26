632f74be1b8c4a54a341ae10317c397848471e9c
pROmeTEuL <radu.teodor.vatra@gmail.com>
2021-10-26 14:50:31 +0300

    .

--- Timber.pro
+++ Timber.pro
@@ -7,3 +7,19 @@ LIBS += -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window
 S
 SOURCES += \
         main.cpp
+
+DISTFILES += \
+    fonts/KOMIKAP_.ttf \
+    graphics/axe.png \
+    graphics/background.png \
+    graphics/bee.png \
+    graphics/branch.png \
+    graphics/cloud.png \
+    graphics/log.png \
+    graphics/player.png \
+    graphics/rip.png \
+    graphics/tree.png \
+    graphics/tree2.png \
+    sound/chop.wav \
+    sound/death.wav \
+    sound/out_of_time.wav
--- main.cpp
+++ main.cpp
@@ -5,52 +5,152 @@ using namespace sf;
 
 int main()
 {
-    VideoMode vm(1280, 720);
-    RenderWindow fereastra(vm, "Timber", Style::Default);
+    VideoMode vm(1920, 900);
+    RenderWindow window(vm, "Timber", Style::Default);
 
     /*
     ******************************
              TEXTURILE
     ******************************
     */
-    Texture texturaPeisaj;
-    texturaPeisaj.loadFromFile("Peisaj.png");
-    Sprite spritePeisaj;
-    spritePeisaj.setTexture(texturaPeisaj);
-    Texture texturaCopac;
-    texturaCopac.loadFromFile("Copac.png");
-    Sprite spriteCopac;
-    spriteCopac.setTexture(texturaCopac);
-    Texture texturaAlbina;
-    texturaAlbina.loadFromFile("Albina.png");
-    Sprite spriteAlbina;
-    spriteAlbina.setTexture(texturaAlbina);
-
+    ///FUNDAL
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+    ///COPAC
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    ///ALBINA
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    ///NORI
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
     /*
     ******************************
              CONFIGURARI
     ******************************
     */
-    spritePeisaj.setPosition(0,0);
-    spriteCopac.setPosition(550, 0);
-    spriteAlbina.setPosition(100, 50);
-    bool albinaActiva = false;
-    float vitezaAlbinei = 0.0;
+    ///FUNDAL
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+    ///COPAC
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+    ///ALBINA
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
+    ///NORI
+    Sprite spriteCloud0;
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    spriteCloud0.setTexture(textureCloud);
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud0.setPosition(0, 0);
+    spriteCloud1.setPosition(0, 250);
+    spriteCloud2.setPosition(0, 500);
+    bool cloud0active = false;
+    bool cloud1active = false;
+    bool cloud2active = false;
+    float cloud0speed = 0.0f;
+    float cloud1speed = 0.0f;
+    float cloud2speed = 0.0f;
+
+    /*
+    ******************************
+
+    ******************************
+    */
+
+    Clock clock;
 
     /*
     ******************************
              BUCLA WHILE
     ******************************
     */
-    while(fereastra.isOpen()){
+    while (window.isOpen()) {
         if(Keyboard::isKeyPressed(Keyboard::Escape)){
-            fereastra.close();
+            window.close();
         }
-        fereastra.clear();
-        fereastra.draw(spritePeisaj);
-        fereastra.draw(spriteCopac);
-        fereastra.draw(spriteAlbina);
-        fereastra.display();
+        Time dt = clock.restart();
+        if (!beeActive) {
+            srand((int)time(0));
+            beeSpeed = (rand() % 200) + 200;
+            srand((int)time(0) * 10);
+            float height = (rand() % 500) + 500;
+            spriteBee.setPosition(2000, height);
+            beeActive = true;
+        }
+        else {
+            spriteBee.setPosition(
+                        spriteBee.getPosition().x -
+                        beeSpeed * dt.asSeconds(),
+                        spriteBee.getPosition().y);
+            if (spriteBee.getPosition().x < -100) {
+                beeActive = false;
+            }
+        }
+        if (!cloud0active) {
+            srand((int)time(0) * 10);
+            cloud0speed = (rand() % 200);
+            srand((int)time(0) * 10);
+            float height = rand() % 150;
+            spriteCloud0.setPosition(-200, height);
+            cloud0active = true;
+        } else {
+            spriteCloud0.setPosition(
+                        spriteCloud0.getPosition().x +
+                        (cloud0speed * dt.asSeconds()),
+                        spriteCloud0.getPosition().y);
+            if (spriteCloud0.getPosition().x > 1920) {
+                cloud0active = false;
+            }
+        }
+        if (!cloud1active) {
+            srand((int)time(0) * 20);
+            cloud1speed = (rand() % 200);
+            srand((int)time(0) * 20);
+            float height = rand() % 300 - 150;
+            spriteCloud1.setPosition(-200, height);
+            cloud1active = true;
+        } else {
+            spriteCloud1.setPosition(
+                        spriteCloud1.getPosition().x +
+                        (cloud1speed * dt.asSeconds()),
+                        spriteCloud1.getPosition().y);
+            if(spriteCloud1.getPosition().x > 1920) {
+                cloud1active = false;
+            }
+        }
+        if (!cloud2active) {
+            srand((int)time(0) * 30);
+            cloud2speed = (rand() % 200);
+            srand((int)time(0) * 30);
+            float height = rand() % 450 - 150;
+            spriteCloud2.setPosition(-200, height);
+            cloud2active = true;
+        } else {
+            spriteCloud2.setPosition(
+                        spriteCloud2.getPosition().x +
+                        cloud2speed * dt.asSeconds(),
+                        spriteCloud2.getPosition().y);
+            if (spriteCloud2.getPosition().x > 1920) {
+                cloud2active = false;
+            }
+        }
+        window.clear();
+        window.draw(spriteBackground);
+        window.draw(spriteCloud0);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+        window.display();
     }
     return 0;
 }
